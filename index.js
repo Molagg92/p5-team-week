74478bee3c6e3d9060f76c90d9d9599cae8a8a89
Kymani Stephens <kymanist00@gmail.com>
2023-04-26 15:08:48 -0700

    comment in index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const sketch = function (p) {
-
-    p.setup = function () {
-    }
-
-    p.draw = function () {
-        
-    };
-};
-
-let container = document.getElementById("sketch-container");
-let myP5 = new p5(sketch, container);
+// This is just an index.js
